c4a9a6a0b45298adb15ba23e6ee4c180c2fe5786
kyfgcodes <kylegasserfernando@gmail.com>
2023-05-01 11:46:34 +0200

    func change

--- all_star_challenge_18(done).py
+++ all_star_challenge_18(done).py
@@ -1,12 +1,13 @@
-'''Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+"""Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
 
-If no occurrences can be found, a count of 0 should be returned.'''
+If no occurrences can be found, a count of 0 should be returned."""
+
 
 def str_count(strng, letter):
     l_count = strng.count(letter)
     return l_count
 
 
-print(str_count('', '1'))
+print(str_count("", "1"))
 
-#Done!!!
+# Done!!!
