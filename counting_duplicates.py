be22cb4371d20a38b519531688058cea27cf36af
kyfgcodes <kylegasserfernando@gmail.com>
2023-05-02 10:33:46 +0200

    still struggling

--- counting_duplicates.py
+++ counting_duplicates.py
@@ -1,19 +1,24 @@
-'''Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.'''
+"""Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits."""
+
 
 def duplicate_count(text):
+    new_text = [x for x in text]  # list comprehension
     dupe_count = 0
-    occurence_count = 0 
-    for letter in text:
-        if text.count(letter) > 1:
+    dupe_list = []
+    occurence_count = 0
+
+    for i in new_text:
+        if new_text.count(i) >= 2:
+            dupe_count += 1
+            dupe_list.append(i)
             occurence_count += 1
-            if occurence_count >= 2:
-                dupe_count += 1
-                occurence_count = 0
+        if occurence_count == 1:
+            dupe_count -= 1
+            occurence_count = 0
+
+    return dupe_count
 
 
-    return dupe_count 
+print(duplicate_count("iiss"))
 
-
-print(duplicate_count("abcdeaB"))
-
-#Struggling with this one
+# Struggling with this one
