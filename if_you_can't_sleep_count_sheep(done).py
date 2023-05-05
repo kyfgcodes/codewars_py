6c1ecc1ee56f97878270fcc420a9cc56fe90e47d
kyfgcodes <kylegasserfernando@gmail.com>
2023-05-05 11:30:36 +0200

    attempted challenge

--- if_you_can't_sleep_count_sheep(done).py
+++ if_you_can't_sleep_count_sheep(done).py
@@ -4,9 +4,10 @@
 def count_sheep(n):
     sheep = ""
 
-    for i in n:
-        sheep += f"{i}...sheep"
+    for i in range(1, n + 1) :
+        sheep += f"{i} sheep..."
     return sheep
 
+print(count_sheep(10))
 
-print(count_sheep(2))
+#Done!!!
