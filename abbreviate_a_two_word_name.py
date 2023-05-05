14e08b81cbba8a086e60e6897398997c8a697c4e
kyfgcodes <kylegasserfernando@gmail.com>
2023-05-05 11:39:50 +0200

    attempted challenge

---
+++ abbreviate_a_two_word_name.py
@@ -0,0 +1,13 @@
+'''Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.'''
+
+def abbrev_name(name):
+    name = name.upper().split()
+    abr = ''
+    for i in name:
+        abr += i[0]
+    return '.'.join(abr)
+
+
+
+
+print(abbrev_name('Aj Galler'))
