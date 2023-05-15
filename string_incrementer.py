f66288c900570b134232864097a7b795735f55f0
kyfgcodes <kylegasserfernando@gmail.com>
2023-05-15 10:30:40 +0200

    attempting

--- string_incrementer.py
+++ string_incrementer.py
@@ -4,34 +4,36 @@ import re
 If the string already ends with a number, the number should be incremented by 1.
 If the string does not end with a number. the number 1 should be appended to the new string.'''
 
+
 def increment_string(strng):
-    # strng = [x for x in strng]
-    # test = []
-    # for i in reversed(strng):
-    #     if i.isalpha():
-    #         break
-
-    #     test.append(i)
-
+    zero_counter = str(strng.count('0'))
     if not strng:
         return '1'
     if not strng[-1].isdigit():
         return f'{strng}1'
     
-    strng = ''
-    num = ''
-    #Zero counter
-
-    for i in reversed(strng):
-        if i.isalpha():
-            break
-        num += i
+    num = []
     
+    for i in reversed(strng):
+       if i.isalpha():
+           break
+       num.append(i)
+    
+    str_num = (''.join([x for x in reversed(num)]))
+    new_strng = strng.replace(str_num, '')
+    num = int(str_num) + 1
+    if num >= 10:
+        less_zero_counter = str(strng.count('0')- 1)
+        zero_fill = new_strng.zfill(len(less_zero_counter))
+    else:
+        zero_fill = new_strng.zfill(len(zero_counter))
+    
+    
+   
+    return f'{zero_fill}{num}',  num, zero_counter
 
-    return  num
 
 
-
-print(increment_string('foo99obar99'))
+print(increment_string('foo008'))
 
 #Struggling
